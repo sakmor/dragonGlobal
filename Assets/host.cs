5e794c003420ce6e491d401496720e107341efe0
sakmor <sakmor@gmail.com>
2017-04-10 09:56:36 +0800

    非常初略版

---
+++ Assets/host.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class host : MonoBehaviour
+{
+    List<int> cardLibrary = new List<int>();
+    int playerNum;
+    // Use this for initialization
+    void Start()
+    {
+        createCardsDouble();
+        shuffleCards();
+        displayCardsLibrary();
+        dealCardto();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void dealCardto()
+    {
+        // GameObject.Find("main").GetComponent<main>().receiveCard(cardLibrary[0], cardLibrary[1]);
+        cardLibrary.RemoveAt(1);
+        cardLibrary.RemoveAt(0);
+    }
+    void createCardsDouble()
+    {
+        createCardsSingle();
+        createCardsSingle();
+    }
+    void createCardsSingle()
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 13; j++)
+            {
+                cardLibrary.Add(i * 13 + j);
+            }
+        }
+    }
+    void shuffleCards()
+    {
+        for (var i = 0; i < cardLibrary.Count; i++)
+        {
+            int r = Random.Range(0, 51);
+            int temp = cardLibrary[r];
+            cardLibrary[r] = cardLibrary[i];
+            cardLibrary[i] = temp;
+        }
+    }
+    void destoryDisplayedCards()
+    {
+        var displayedCards = GameObject.FindGameObjectsWithTag("cardsLibrary");
+        if (displayedCards.Length > 0)
+        {
+            foreach (var i in displayedCards)
+            {
+                DestroyImmediate(i);
+            }
+        }
+    }
+    void displayCardsLibrary()
+    {
+        GameObject card = GameObject.Find("card");
+        Sprite[] cardsSprite = Resources.LoadAll<Sprite>(@"poker");
+        int count = 0;
+        for (var i = 0; i < cardLibrary.Count; i++)
+        {
+            GameObject k = Instantiate(card);
+            k.transform.tag = "cardsLibrary";
+            int spriteIndex = decode2Index(cardLibrary[i]);
+            k.GetComponent<SpriteRenderer>().sprite = cardsSprite[spriteIndex];
+            k.name = k.GetComponent<SpriteRenderer>().sprite.name;
+            k.transform.position += new Vector3(count * 2, 0, 0);
+            count++;
+        }
+    }
+    void refreshDisplayCardsLibrary()
+    {
+        destoryDisplayedCards();
+        displayCardsLibrary();
+    }
+    string decode2Sting(int n)
+    {
+        string[] suit = { "club", "diamond", "heart", "spade" };
+        string nSuit = suit[n / 100];
+        string num = (n % 100).ToString("00");
+        return nSuit + num;
+    }
+    int decode2Index(int n)
+    {
+        return n / 100 * 13 + n % 100;
+    }
+}
---
+++ Assets/main.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+
+    Sprite[] cardsSprite;
+    public int hostMessage;
+    int cardLeft, cardRight;
+    bool waitHost;
+    GameObject cardA, cardB, cardC;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(waitHost);
+        GameObject player01 = GameObject.Find("player01");
+        cardA = GameObject.Find("mycard");
+        // cardB = player01.transform.Find("cardRight").gameObject;
+        cardsSprite = Resources.LoadAll<Sprite>(@"poker");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contorl();
+        receiveHost();
+    }
+    void receiveHost()
+    {
+    }
+    void actionByhostMessage()
+    {
+        int actionType = hostMessage % 1000;
+        switch (actionType)
+        {
+            case 1:
+                //host發牌
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+        }
+
+    }
+    void receiveCard(int a, int b)
+    {
+        cardLeft = a;
+        cardRight = b;
+        displayCard();
+    }
+    void displayCard()
+    {
+        int spriteIndex = decode2Index(cardLeft);
+        GameObject.Find("mycard").transform.position += Vector3.up;
+        cardA.GetComponent<SpriteRenderer>().sprite = cardsSprite[spriteIndex];
+        // spriteIndex = decode2Index(cardRight);
+        // cardB.GetComponent<SpriteRenderer>().sprite = cardsSprite[spriteIndex];
+    }
+    int decode2Index(int n)
+    {
+        return n / 100 * 13 + n % 100;
+    }
+
+
+
+    void contorl()
+    {
+
+        if (Input.GetKeyUp("space"))
+        {
+
+        }
+        if (Input.GetKeyUp("f"))
+        {
+
+        }
+    }
+
+
+
+
+
+
+}
